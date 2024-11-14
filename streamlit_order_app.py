ebe6d9c46dfdec9a01b826aa7236c83ffb428ef2
yellivenu <yellivenu@gmail.com>
2024-11-14 16:17:03 +0530

    Update streamlit_order_app.py

--- streamlit_order_app.py
+++ streamlit_order_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session (only in snowflake sis)
 from snowflake.snowpark.functions import col,when_matched
 # Write directly to the app
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
@@ -8,8 +8,9 @@ st.write(
     """   Orders that need to be filled
     """
 )
-
-session = get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+#session = get_active_session() --only in snowflake (sis)
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 if my_dataframe:
     editable_df=st.data_editor(my_dataframe)
